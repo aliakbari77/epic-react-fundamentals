891321e41fd6763a17210692f6ad8398acfad73f
ali <7731.aa@gmail.com>
2023-12-08 10:25:18 +0330

    complete exercise 5

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,8 +1,8 @@
 // Styling
 // http://localhost:3000/isolated/exercise/05.js
 
-import * as React from 'react'
-import '../box-styles.css'
+import * as React from "react";
+import "../box-styles.css";
 
 // 🐨 add a className prop to each div and apply the correct class names
 // based on the text content
@@ -14,18 +14,31 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+function Box({ style, size, className = "", ...otherProps }) {
+  const sizeClassName = size ? `box--${size}` : "";
+  return (
+    <div
+      className={`box ${className} ${sizeClassName}`}
+      style={{ fontStyle: "italic", ...style }}
+      {...otherProps}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box size={"small"} style={{ backgroundColor: "lightblue" }}>
+        small lightblue box
+      </Box>
+      <Box size={"medium"} style={{ backgroundColor: "pink" }}>
+        medium pink box
+      </Box>
+      <Box size={"large"} style={{ backgroundColor: "orange" }}>
+        large orange box
+      </Box>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
